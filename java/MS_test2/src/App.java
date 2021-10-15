0162bd4ed04bfac8180a309ce704d35c202da6d1
Carlos González Espadas <cgonzalez21@ilg.cat>
2021-10-15 17:55:41 +0000

    "vscode nivell1: Carlos Gonzalez"

---
+++ java/MS_test2/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\nEscriu el teu nom: ");
+        String nom = in.nextLine();
+        System.out.println("\nHola " + nom + "!!\n");
+        in.close();
+        
+    }
+
+}
